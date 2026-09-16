71887fc4a9f6ab0a86548d9e272b52ae7354cb7f
chrehall68 <chrehall68@gmail.com>
2026-09-16 12:55:38 -0700

    remove shared library

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,10 +10,10 @@ struct TcpClientCli {
     #[arg(short, long)]
     verbose: bool,
     /// the port to connect to for tcp connections
-    #[arg(long, default_value_t = discord_common::DEFAULT_TCP_PORT)]
+    #[arg(long)]
     tcp_port: u16,
     /// the host to connect to
-    #[arg(long, default_value_t = discord_common::DEFAULT_HOST.to_string())]
+    #[arg(long)]
     host: String,
 }
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -11,13 +11,13 @@ struct ServerCli {
     #[arg(short, long)]
     verbose: bool,
     /// the port to listen for http connections on
-    #[arg(long, default_value_t = discord_common::DEFAULT_HTTP_PORT)]
+    #[arg(long)]
     http_port: u16,
     /// the port to listen for regular tcp connections on
-    #[arg(long, default_value_t = discord_common::DEFAULT_TCP_PORT)]
+    #[arg(long)]
     tcp_port: u16,
     /// the host to listen on
-    #[arg(long, default_value_t = discord_common::DEFAULT_HOST.to_string())]
+    #[arg(long)]
     host: String,
 }
 
